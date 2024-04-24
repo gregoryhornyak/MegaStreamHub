c26e133ce89fc512b69d3aae9eaef39f19137ba4
Gergely Hornyak <gergely2125@gmail.com>
2024-04-24 21:16:08 +0100

    added core, media, and html

---
+++ core.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('menu.html')
+
+@app.route('/barber')
+def barber():
+    return render_template('barber.html')
+
+@app.route('/cosmos')
+def cosmos():
+    return render_template('cosmos.html')
+
+@app.route('/coffee')
+def coffee():
+    return render_template('coffee.html')
+
