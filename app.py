3769242d0f9eb8cf1a69a3c7579a4d44d3b3bd5a
Gergely Hornyak <gergely2125@gmail.com>
2024-04-25 18:42:40 +0100

    added gitignore, and updated to app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('menu.html')
+    return render_template("menu.html")
 
 @app.route('/barber')
 def barber():
@@ -18,3 +18,5 @@ def cosmos():
 def coffee():
     return render_template('coffee.html')
 
+if __name__ == "__main__":
+    app.run()
